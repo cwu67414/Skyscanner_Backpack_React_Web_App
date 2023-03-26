3add54ba360932d61b0740a6279af8295bc3c3ea
cwu67414 <cwu67414@usc.edu>
2023-03-26 01:03:22 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { BpkCode } from '@skyscanner/backpack-web/bpk-component-code';
+import BpkButton from '@skyscanner/backpack-web/bpk-component-button';
+import BpkText from '@skyscanner/backpack-web/bpk-component-text';
+import BpkCalendar from '@skyscanner/backpack-web/bpk-component-calendar';
+import BpkInput, {
+  INPUT_TYPES,
+} from '@skyscanner/backpack-web/bpk-component-input';
+import format from 'date-fns/format';
+
+import { cssModules } from '@skyscanner/backpack-web/bpk-react-utils';
+
+import STYLES from './App.scss';
+
+const getClassName = cssModules(STYLES);
+
+const formatDateFull = (date) => format(date, 'EEEE, do MMMM yyyy');
+const formatMonth = (date) => format(date, 'MMMM yyyy');
+const daysOfWeek = [
+  {
+    name: 'Sunday',
+    nameAbbr: 'Sun',
+    index: 0,
+    isWeekend: true,
+  },
+  // ...
+];
+
+const App = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateSelect = (date) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <div className={getClassName('App')}>
+      <header className={getClassName('App__header')}>
+        <div className={getClassName('App__header-inner')}>
+          <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>
+            Flight Schedule
+          </BpkText>
+        </div>
+      </header>
+      <main className={getClassName('App__main')}>
+        <BpkInput
+          id="dateInput"
+          type={INPUT_TYPES.text}
+          name="date"
+          value={(selectedDate || '').toString()}
+          placeholder="Departure date"
+        />
+        <BpkCalendar
+          id="calendar"
+          onDateSelect={handleDateSelect}
+          formatMonth={formatMonth}
+          formatDateFull={formatDateFull}
+          daysOfWeek={daysOfWeek}
+          weekStartsOn={1}
+          changeMonthLabel="Change month"
+          nextMonthLabel="Next month"
+          previousMonthLabel="Previous month"
+          selectionConfiguration={{
+            type: 'single',
+            date: selectedDate,
+          }}
+          className={getClassName('App__calendar')}
+        />
+        <BpkButton onClick={() => alert('It works!')}>Continue</BpkButton>
+      </main>
+    </div>
+  );
+};
+
+export default App;
